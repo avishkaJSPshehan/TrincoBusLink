4ea9631cf5884f516e41347c94081d4c29337c37
JAYASIRI S.P.A.S. <ict21860@fot.sjp.ac.lk>
2024-09-15 23:37:48 +0530

    Working on Home Section

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./home.css";
 import video from "../../Assets/home_video.mp4";
 import { GrLocation } from "react-icons/gr";
+import { HiFilter } from "react-icons/hi";
 
 const Home = () => {
   return (
@@ -16,13 +17,38 @@ const Home = () => {
         </div>
 
         <div className="cardDiv grid">
-          <div className="destinationInput">
+
+          <div className="fromInput">
             <label htmlFor="city">Where Are You Now:</label>
             <div className="input flex">
               <input type="text" placeholder="From..." />
               <GrLocation className="icon" />
             </div>
           </div>
+
+          <div className="destinationInput">
+            <label htmlFor="city">Where You Want to Go:</label>
+            <div className="input flex">
+              <input type="text" placeholder="To..." />
+              <GrLocation className="icon" />
+            </div>
+          </div>
+
+          <div className="dateInput">
+            <label htmlFor="date">Travel Date:</label>
+            <div className="input flex">
+              <input type="date"/>
+            </div>
+          </div>
+
+          <div className="searchOptions flex">
+            <HiFilter className="icon"/>
+            <span>MORE FILTERS</span>
+          </div>
+        </div>
+
+        <div className="homeFooterIcons flex">
+            
         </div>
       </div>
     </section>
