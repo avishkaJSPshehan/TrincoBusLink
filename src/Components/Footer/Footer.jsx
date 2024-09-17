cbec2f490b6f9e477d8dbb37a68ff616b3b68ced
JAYASIRI S.P.A.S. <ict21860@fot.sjp.ac.lk>
2024-09-17 16:39:11 +0530

    Finish Mobile View

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import './footer.css'
 import video2 from '../../Assets/footer_video.mp4'
 import { FiChevronRight, FiSend } from 'react-icons/fi'
 import { MdOutlineTravelExplore } from 'react-icons/md'
 import { AiFillFacebook, AiFillInstagram, AiFillTikTok, AiFillYoutube, AiOutlineTwitter } from 'react-icons/ai'
 
+import Aos from 'aos'
+import 'aos/dist/aos.css'
 
 const Footer = () => {
+
+  useEffect(() => {
+    Aos.init({duration:2000})
+  },[])
+
+
   return (
     <section className='footer'>
       <div className='videoDiv'>
@@ -15,14 +23,14 @@ const Footer = () => {
 
       <div className='secContent container'>
         <div className='contactDiv flex'>
-          <div className='text'>
+          <div data-aos="fade-up" className='text'>
             <small>KEEP IN TOUCH</small>
             <h2>Travel with us</h2>
           </div>
 
           <div className='inputDiv flex'>
-            <input type='text' placeholder='Enter Email Address'/>
-            <button className='btn flex' type='submit'>
+            <input data-aos="fade-up" type='text' placeholder='Enter Email Address'/>
+            <button data-aos="fade-up" className='btn flex' type='submit'>
               SEND <FiSend className='icon'/>
             </button>
           </div>
@@ -36,7 +44,7 @@ const Footer = () => {
               </a>
             </div>
 
-            <div className='footerParagraph'>
+            <div data-aos="fade-up" className='footerParagraph'>
             Lorem Ipsum is simply dummy text of the printing and 
             typesetting industry. Lorem Ipsum has been the industry's 
             standard dummy text ever since the 1500s, when an unknown 
@@ -44,7 +52,7 @@ const Footer = () => {
             specimen book.
             </div>
 
-            <div className='footerSocials flex'>
+            <div data-aos="fade-up" className='footerSocials flex'>
               <AiOutlineTwitter className='icon'/>
               <AiFillInstagram className='icon'/>
               <AiFillFacebook className='icon'/>
@@ -55,7 +63,7 @@ const Footer = () => {
           <div className='footerLinks grid'>
 
             {/*Group One*/}
-            <div className='linkGroup'>
+            <div data-aos="fade-up" data-aos-duration="5000" className='linkGroup'>
               <span className='groupTitle'>
                 OUR AGENCY
               </span>
@@ -87,69 +95,74 @@ const Footer = () => {
             </div>
 
             {/*Group Two*/}
-            <div className='linkGroup'>
+            <div data-aos="fade-up" data-aos-duration="5000" className='linkGroup'>
               <span className='groupTitle'>
-                OUR AGENCY
+                PARTNERS
               </span>
 
               <li className='footerList flex'>
                 <FiChevronRight className='icon'/>
-                Services
+                Bookings
               </li>
 
               <li className='footerList flex'>
                 <FiChevronRight className='icon'/>
-                Insurance
+                Rentcars
               </li>
 
               <li className='footerList flex'>
                 <FiChevronRight className='icon'/>
-                Agency
+                HostelWorld
               </li>
 
               <li className='footerList flex'>
                 <FiChevronRight className='icon'/>
-                Tourism
+                Trivago
               </li>
 
               <li className='footerList flex'>
                 <FiChevronRight className='icon'/>
-                Payment
+                TripAdvisor
               </li>
             </div>
 
             {/*Group Three*/}
-            <div className='linkGroup'>
+            <div data-aos="fade-up" data-aos-duration="5000" className='linkGroup'>
               <span className='groupTitle'>
-                OUR AGENCY
+                LAST MINUTE
               </span>
 
               <li className='footerList flex'>
                 <FiChevronRight className='icon'/>
-                Services
+                London
               </li>
 
               <li className='footerList flex'>
                 <FiChevronRight className='icon'/>
-                Insurance
+                California
               </li>
 
               <li className='footerList flex'>
                 <FiChevronRight className='icon'/>
-                Agency
+                Indonesia
               </li>
 
               <li className='footerList flex'>
                 <FiChevronRight className='icon'/>
-                Tourism
+                Europe
               </li>
 
               <li className='footerList flex'>
                 <FiChevronRight className='icon'/>
-                Payment
+                Oceania
               </li>
             </div>
 
+          </div>
+
+          <div className='footerDiv flex'>
+            <small>BEST TRAVEL WEBSITE</small>
+            <small>COPYRIGHTS RESERVED    JSPDev 2024</small>
           </div>
         </div>
       </div>
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React,{useEffect} from "react";
 import "./home.css";
 import video from "../../Assets/home_video.mp4";
 import { GrLocation } from "react-icons/gr";
 import { HiFilter } from "react-icons/hi";
 
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+
 const Home = () => {
+
+  useEffect(() => {
+    Aos.init({duration:2000})
+  },[])
+
+
+
   return (
     <section className="home">
       <div className="overlay"></div>
@@ -12,11 +22,11 @@ const Home = () => {
 
       <div className="homeContent container">
         <div className="textDiv">
-          <span className="smallText">Search Your Journey</span>
-          <h1 className="homeTitle">Online Seat Reservation</h1>
+          <span data-aos="fade-up" className="smallText">Search Your Journey</span>
+          <h1 data-aos="fade-up" className="homeTitle">Online Seat Reservation</h1>
         </div>
 
-        <div className="cardDiv grid">
+        <div data-aos="fade-up" className="cardDiv grid">
 
           <div className="fromInput">
             <label htmlFor="city">Where Are You Now:</label>
@@ -43,7 +53,7 @@ const Home = () => {
 
           <div className="searchOptions flex">
             <HiFilter className="icon"/>
-            <span>MORE FILTERS</span>
+            <span>FIND A BUS</span>
           </div>
         </div>
 
